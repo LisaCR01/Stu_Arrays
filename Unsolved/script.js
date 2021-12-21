ebfca45b603180ac481d9035560296f2531ecc00
Lisa Gunn <lisagunn@Lisas-MacBook-Pro.local>
2021-12-21 11:59:19 +0000

    update

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -1,1 +1,18 @@
 // WRITE YOUR CODE HERE
+
+var students = ["Rob", "Amelie", "Will", "Muni", "Mark", "Eva"]
+
+console.log(students.length)
+
+console.log("Welcome to the class " + students[0]);
+console.log("Welcome to the class " + students[1]);
+console.log("Welcome to the class " + students[2]);
+console.log("Welcome to the class " + students[3]);
+console.log("Welcome to the class " + students[4]);
+console.log("Welcome to the class " + students[5]);
+
+Student [0] = 'Rafe'
+
+if (students[0] === "Rafe") {
+    console.log(students[0] + " is in class!");
+} 
